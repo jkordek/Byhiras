35cfc5ba5780277e3d89193160c8a77832312fd3
jkordek <j.kordek56@gmail.com>
2020-01-11 22:59:45 +0000

    added monestrs names

--- src/common/assets/monsterName.js
+++ src/common/assets/monsterName.js
@@ -1,42 +1,42 @@
 const monsterName = [
   {
-    'id': '1',
+    'id': 1,
     'name': 'Qulbaym, The Clumsy One',
   },
   {
-    'id': '2',
+    'id': 2,
     'name': 'Granacryss, Champion Of The Red',
   },
   {
-    'id': '3',
+    'id': 3,
     'name': 'Brilbisdeon, The Gifted',
   },
   {
-    'id': '4',
+    'id': 4,
     'name': 'Dokdreg Kragfeet',
   },
   {
-    'id': '5',
+    'id': 5,
     'name': 'Gikrirlum Blackgranite',
   },
   {
-    'id': '6',
+    'id': 6,
     'name': 'Mezilkree',
   },
   {
-    'id': '7',
+    'id': 7,
     'name': 'Hyptu',
   },
   {
-    'id': '8',
+    'id': 8,
     'name': 'Zufem',
   },
   {
-    'id': '9',
+    'id': 9,
     'name': 'Respen Carnelis',
   },
   {
-    'id': '10',
+    'id': 10,
     'name': 'Cyran Valpeiros',
   }
 ];
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import HealthBar from '../HealthBar/HealthBar';
+import monsterName from '../../common/assets/monsterName';
 
 import './Player.scss';
 
@@ -11,17 +12,28 @@ type args = {
   diceTwo:Number,
 }
 
+let randomId = Math.floor(Math.random() * 10) + 1;
+
 const Player = (args) => (
-    <div className='playerContainer'>
-      <span className='playerType'>{args.playerType}</span>
-      <div className='playerHP'>
-        <HealthBar hpValue={args.hp} />
-      </div>
-      <div className='playerDice'>
-        <span>First Dice: {args.diceOne}</span>
-        <span>Second Dice: {args.diceTwo}</span>
-      </div>
+  <div className='playerContainer'>
+    <span className='playerType'>{
+      args.playerType === 'Player' ?
+        args.playerType :
+        monsterName.map(name => {
+          if(randomId === name.id) {
+            return name.name
+          }
+        })
+    }
+    </span>
+    <div className='playerHP'>
+      <HealthBar hpValue={args.hp} />
     </div>
+    <div className='playerDice'>
+      <span>First Dice: {args.diceOne}</span>
+      <span>Second Dice: {args.diceTwo}</span>
+    </div>
+  </div>
 )
 
 export default Player;
